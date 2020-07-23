3f5647540a460773e397e60c5cfb6f491be65000
flaviogf <flavio.fernandes6@gmail.com>
2020-07-23 15:58:49 -0300

    feat: searching states and cities in a recursive way, and adding output Gateway that export data to CSV file

--- src/zip-range.js
+++ src/zip-range.js
@@ -1,9 +1,13 @@
-import querystring from 'querystring'
 import axios from 'axios'
 import chalk from 'chalk'
 import cheerio from 'cheerio'
+import fs from 'fs'
+import uniqBy from 'lodash/uniqBy'
+import path from 'path'
+import querystring from 'querystring'
+import { promisify } from 'util'
 
-function zipRange() {
+export default function zipRange(outputGateway) {
   const client = createClient()
 
   const correios = createCorreios(client)
@@ -11,8 +15,41 @@ function zipRange() {
   return correios
     .fetchStates()
     .then(correios.fetchCities)
-    .then((cities) => console.log(chalk.green(JSON.stringify(cities))))
-    .catch((err) => console.log(chalk.red(err)))
+    .then(outputGateway.execute)
+    .then(() => console.log(chalk.green('States have been exported')))
+    .catch((err) => console.log(chalk.red('States have not been exported', err)))
+}
+
+export function createCSVFileOutputGateway(directory) {
+  function execute(states) {
+    return createOutputFolder().then(() => Promise.all([saveStates(states), ...states.map(saveCities)]))
+  }
+
+  function createOutputFolder() {
+    return promisify(fs.exists)(directory).then((exists) => !exists && promisify(fs.mkdir)(directory))
+  }
+
+  function saveStates(states) {
+    const header = 'id,name,beginZipCode,endZipCode\n'
+
+    const data = states.map((it) => `${it.id},${it.name},${it.beginZipCode},${it.endZipCode}\n`).join('')
+
+    return promisify(fs.writeFile)(path.join(directory, 'states.csv'), `${header}${data}`)
+  }
+
+  function saveCities(state) {
+    const header = 'name,stateId,stateName,beginZipCode,endZipCode\n'
+
+    const data = state.cities
+      .map((it) => `${it.name},${it.stateId},${it.stateName},${it.beginZipCode},${it.endZipCode}\n`)
+      .join('')
+
+    return promisify(fs.writeFile)(path.join(directory, `${state.name}.csv`), `${header}${data}`)
+  }
+
+  return {
+    execute,
+  }
 }
 
 function createClient() {
@@ -24,8 +61,7 @@ function createClient() {
 
 function createCorreios(client) {
   function fetchStates() {
-    const url =
-      'http://www.buscacep.correios.com.br/sistemas/buscacep/buscaFaixaCep.cfm'
+    const url = 'http://www.buscacep.correios.com.br/sistemas/buscacep/buscaFaixaCep.cfm'
 
     return client
       .get(url)
@@ -38,24 +74,27 @@ function createCorreios(client) {
 
             return hasValue
           })
-          .map((_, it) => {
+          .map((index, it) => {
+            const id = index + 1
+
             const name = $(it).text()
 
             return {
+              id,
               name,
             }
           })
           .toArray()
       })
+      .then(fetchZipCodeRangeOfTheStates)
   }
 
-  function fetchCities(states) {
-    return Promise.all(states.map(fetchCitiesByState))
+  function fetchZipCodeRangeOfTheStates(states) {
+    return Promise.all(states.map(fetchZipCodeRangeOfTheState))
   }
 
-  function fetchCitiesByState(state) {
-    const url =
-      'http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaFaixaCEP.cfm'
+  function fetchZipCodeRangeOfTheState(state) {
+    const url = 'http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaFaixaCEP.cfm'
 
     const data = querystring.stringify({
       UF: state.name,
@@ -65,8 +104,61 @@ function createCorreios(client) {
       .post(url, data)
       .then((res) => res.data.toString('latin1'))
       .then((data) => cheerio.load(data, { decodeEntities: false }))
-      .then(($) =>
-        $('table:last-of-type tr')
+      .then(($) => {
+        const beginZipCode = $('table:first-of-type tr')
+          .find('td:nth-child(2)')
+          .text()
+          .trim()
+          .match(/^\d{5}-\d{3}/g)
+          .toString()
+          .replace(/(\d{5})-(\d{3})/g, '$1$2')
+
+        const endZipCode = $('table:first-of-type tr')
+          .find('td:nth-child(2)')
+          .text()
+          .trim()
+          .match(/\d{5}-\d{3}$/g)
+          .toString()
+          .replace(/(\d{5})-(\d{3})/g, '$1$2')
+
+        return {
+          ...state,
+          beginZipCode,
+          endZipCode,
+        }
+      })
+  }
+
+  function fetchCities(states) {
+    return Promise.all(states.map((it) => browseAllCityPages(it).then((cities) => ({ ...it, cities }))))
+  }
+
+  function browseAllCityPages(state, cities = [], next = { begin: 1, end: 50 }) {
+    return paginatedCitiesByState(state, next).then((page) => {
+      const newCities = uniqBy([...cities, ...page.content], 'name')
+
+      if (!page.next) return Promise.resolve(newCities)
+
+      return browseAllCityPages(state, newCities, page.next)
+    })
+  }
+
+  function paginatedCitiesByState(state, { begin, end }) {
+    const url = 'http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaFaixaCEP.cfm'
+
+    const data = querystring.stringify({
+      UF: state.name,
+      qtdrow: 50,
+      pagini: begin,
+      pagfim: end,
+    })
+
+    return client
+      .post(url, data)
+      .then((res) => res.data.toString('latin1'))
+      .then((data) => cheerio.load(data, { decodeEntities: false }))
+      .then(($) => {
+        const cities = $('table:last-of-type tr')
           .filter((_, it) => {
             const hasChildren = $(it).find('td').length
 
@@ -86,12 +178,37 @@ function createCorreios(client) {
 
             return {
               name,
+              stateId: state.id,
+              stateName: state.name,
               beginZipCode,
               endZipCode,
             }
           })
-          .toArray(),
-      )
+          .toArray()
+
+        const next = $('form[name=Proxima]').serialize()
+
+        if (!next) {
+          return {
+            content: cities,
+            next: null,
+          }
+        }
+
+        const urlSearchParams = new URLSearchParams(next)
+
+        const begin = urlSearchParams.get('pagini')
+
+        const end = urlSearchParams.get('pagfim')
+
+        return {
+          content: cities,
+          next: {
+            begin,
+            end,
+          },
+        }
+      })
   }
 
   return {
@@ -99,5 +216,3 @@ function createCorreios(client) {
     fetchCities,
   }
 }
-
-export default zipRange
