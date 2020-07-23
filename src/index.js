24ca0fa6694c09e5cf7ccb4ac50eafc61db7bd5a
flaviogf <flavio.fernandes6@gmail.com>
2020-07-23 16:01:13 -0300

    feat: using zip-range with csvOutputGateway

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import zipRange from './zip-range'
+import path from 'path'
+import zipRange, { createCSVFileOutputGateway } from './zip-range'
 
-zipRange()
+const directory = path.join(__dirname, '..', 'out')
+
+const CSVFileOutputGateway = createCSVFileOutputGateway(directory)
+
+zipRange(CSVFileOutputGateway)
